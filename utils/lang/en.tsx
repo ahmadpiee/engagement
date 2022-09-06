d12779c9ba7bdc09986efc0f7b3cb714b1393643
ahmadpiee <ahmadpiee69@gmail.com>
2022-09-06 21:27:56 +0700

    modify footer text

--- utils/lang/en.tsx
+++ utils/lang/en.tsx
@@ -10,7 +10,7 @@ const en = {
   leftMenu: "Sofyan and Mutiara",
   prolouge:
     "dear my love, I think i'm the only one who know exactly how big this feeling for you, i know i'm not perfect, but i hope you have someone who always back you up and protect you either you happy or sad, it doesn't mean you need to, it means i don't want you face it alone, because this feeling just can't stop falling in love with you, and let me be your man and part of your journey, will you?",
-  footer: "Sincerely Made By Your Man ❤️",
+  footer: "Sincerely Made By ❤️ & Coffee",
   gallery: "Collage",
   intro: "Intro",
   video: "Documentaion",
--- utils/lang/id.tsx
+++ utils/lang/id.tsx
@@ -10,7 +10,7 @@ const id = {
   leftMenu: "Sofyan dan Mutiara",
   prolouge:
     "dear my love, I think i'm the only one who know exactly how big this feeling for you, i know i'm not perfect, but i hope you have someone who always back you up and protect you either you happy or sad, it doesn't mean you need to, it means i don't want you face it alone, because this feeling just can't stop falling in love with you, and let me be your man and part of your journey, will you?",
-  footer: "Dibuat Dengan Tulus Oleh Lelakimu ❤️",
+  footer: "Dibuat Dengan Tulus Oleh ❤️ & Coffee",
   gallery: "Kolase",
   intro: "Intro",
   video: "Dokumentasi",
